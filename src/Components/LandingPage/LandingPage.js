8eb7d7c739035326283a628df63038e48ac9828e
Ryan Hikel <hikelryan@gmail.com>
2019-10-13 00:39:49 -0500

    small homepage update for title

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -4,7 +4,15 @@ import './LandingPage.css';
 class LandingPage extends Component {
   render() {
     return (
-      <div className='LandingPage'>
+      <div className="LandingPage">
+        <div className="splash-title">
+          <h1>
+            Healthy Kids
+          </h1>
+          <h3>
+            Looking out for what matters to you.
+          </h3>
+        </div>
       </div>
     )
   }
